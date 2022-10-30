65d23eef21136703b9ec24ba2578c9c3bbbabf2a
tia <tiakristensalmon@gmail.com>
2022-10-30 09:46:06 -0600

    Added js functionality for newsletter section

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 /* Add your JavaScript to this file */
+window.addEventListener('DOMContentLoaded', () => {
+    const message = document.querySelector('.message');
+    const submitBtn = document.querySelector('.newsletter .btn');
+    const input = document.querySelector('.form-group input');
+
+    submitBtn.addEventListener('click', ()=>{
+        let msg = input.value;
+        message.innerHTML = input.validity.valid ? `Thank you! Your email address ${msg} has been added to our mailing list!` : 'Please enter a valid email address.';
+    })
+});
