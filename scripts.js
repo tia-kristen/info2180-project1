e98370a22c4e9ff4f3fd65766a97b3a2c46acf1e
tia <tiakristensalmon@gmail.com>
2022-10-30 17:03:44 -0600

    Updated js to prevent form from redirecting on submit

--- scripts.js
+++ scripts.js
@@ -4,7 +4,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const submitBtn = document.querySelector('.newsletter .btn');
     const input = document.querySelector('.form-group input');
 
-    submitBtn.addEventListener('click', ()=>{
+    submitBtn.addEventListener('click', (e)=>{
+        e.preventDefault();
         let msg = input.value;
         message.innerHTML = input.validity.valid ? `Thank you! Your email address ${msg} has been added to our mailing list!` : 'Please enter a valid email address.';
     })
